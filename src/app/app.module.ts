d5948c01c628f98980ddfae1dde75ebbdc624338
Clement Fourage <clement-fourage@hotmail.fr>
2018-09-25 16:07:07 +0200

    Menu Collaborateur Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { AuthComponent } from './auth/auth.component';
 import {FormsModule} from "@angular/forms";
 import {StatutConnecteService} from "./auth/statut-connecte.service";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import { MenuCollaborateurComponent } from './menu-collaborateur/menu-collaborateur.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
   { path:'tech', component: TechComponent, canActivate:[StatutConnecteService]}, // /tech accessible uniquement si connecté
   { path:'auth', component: AuthComponent},
-  { path: '', redirectTo: '/tech', pathMatch: 'full'}
+  { path: '', redirectTo: '/accueil', pathMatch: 'full'}
 ];
 
 
@@ -22,7 +25,9 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     TechComponent,
-    AuthComponent
+    AuthComponent,
+    MenuCollaborateurComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-collaborateur/menu-collaborateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-collaborateur',
+  templateUrl: './menu-collaborateur.component.html',
+  styleUrls: ['./menu-collaborateur.component.scss']
+})
+export class MenuCollaborateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
